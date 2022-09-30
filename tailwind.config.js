abd5d6ae29dffe4f30641bb25ee1aa5f3aa1c553
kumesh1997 <tharindukumesh09@gmail.com>
2022-10-01 00:47:40 +0530

    Bootstrap was installed

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "lms-green": "#84C873",
+        "lms-light-green":"#C2EE7F",
+        "lms-dark-blue":"#285A84",
+        "lms-light-blue":"#EEF6F3",
+        "lms-black":"#000101",
+        "lms-white":"#FFFFFF"
+      },
+    },
+  },
+  plugins: [],
+}
