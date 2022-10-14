4bb0dfc08c6d372f129db9db20af25b600ba7606
kumesh1997 <tharindukumesh09@gmail.com>
2022-10-14 22:30:42 +0530

    Get Marks endpoint integration done by Kumesh

--- src/pages/ShowMarks.js
+++ src/pages/ShowMarks.js
@@ -1,24 +1,49 @@
 import React from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import axios from '../axios';
+
 
 const ShowMarks = () => {
-  const marks = [
-    {
-      course: 'Information System Modelling',
-      marks: 87,
-    },
-    {
-      course: 'Operation Management',
-      marks: 90,
-    },
-    {
-      course: 'Data Stuctures & Algorithms',
-      marks: 56,
-    },
-  ];
+  // const marks = [
+  //   {
+  //     course: 'Information System Modelling',
+  //     marks: 87,
+  //   },
+  //   {
+  //     course: 'Operation Management',
+  //     marks: 90,
+  //   },
+  //   {
+  //     course: 'Data Stuctures & Algorithms',
+  //     marks: 56,
+  //   },
+  // ];
+
+  const [marks, setMarks] = useState([]);
+  const [stdName, setStdName] = useState("");
+
+  useEffect (() =>{
+    const getMarks = async () => {
+      try {
+        let awaiVar = await axios.get('http://localhost:3006/marks/view/1') 
+        .then(function (response) {
+          setStdName(response.data[0].name);
+          setMarks(response.data);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+      } catch (err) {
+        console.log(err);
+      }
+  }
+  getMarks();
+});
 
   return (
     <div className=' p-20'>
-      <h2>Student Name</h2>
+      <h2>{stdName}</h2>
       <table class='table'>
         <thead class='thead-dark'>
           <tr>
@@ -27,10 +52,10 @@ const ShowMarks = () => {
           </tr>
         </thead>
         <tbody>
-          {marks.map((student) => {
+          {marks.map((student,key) => {
             return (
-              <tr>
-                <td>{student.course}</td>
+              <tr key={key}>
+                <td>{student.courseName}</td>
                 <td>{student.marks}</td>
               </tr>
             );
