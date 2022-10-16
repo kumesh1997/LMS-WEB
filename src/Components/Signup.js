597947b6432585567b087e516a4aed69f7f05b0a
kumesh1997 <tharindukumesh09@gmail.com>
2022-10-16 13:01:09 +0530

    Integration commit 2 in registration form

--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Avatar, Button, Grid, Paper, TextField, Typography } from "@material-ui/core";
+import axios from 'axios';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
@@ -13,9 +14,19 @@ import FormGroup from '@mui/material/FormGroup';
 import MenuItem from '@mui/material/MenuItem';
 import InputLabel from '@mui/material/InputLabel';
 import Select from '@mui/material/Select';
+import { useState } from "react";
 
 
 const Signup = () => {
+
+    const [role, setRole] = useState("");
+    const [name, setName] = useState("");
+    const [email, SetEmail] = useState("");
+    const [gender, setGender] = useState("");
+    const [phone, setPhone] = useState("");
+    const [address, setAddress] = useState("");
+    const [password, setPassword] = useState("");
+
     const paperStyle = { padding: 20, height: '90vh', width: 400, margin: "auto" }
     const avatarStyle = { backgroundColor: '#3ac6a2' }
     const phnStyle = { marginTop: 5 }
@@ -50,12 +61,34 @@ const Signup = () => {
         console.log(props)
     }
     const marginTop = { marginTop: 8 }
-    const [role, setRole] = React.useState('');
+  
 
   const handleChange = (event) => {
     setRole(event.target.value);
   };
    
+
+  const handleOnSubmit= () => {
+    const registerUser = async () => {
+        let dataste = await axios.post('http://localhost:3006/user/register',
+        {  
+            "name" : name,
+            "email" : email,
+            "mobileNumebr" : phone,
+            "address" : address,
+            "password" : password,
+            "birthday" : "",
+            "gender" : gender,
+            "role" : role
+          
+        })
+        .then((response) => {
+          
+        })
+        .catch((error) => console.log(error));
+      };
+      registerUser();
+  }
     return (
         <Grid>
             <Paper style={paperStyle}>
@@ -66,7 +99,7 @@ const Signup = () => {
                 </Grid>
                 <Formik initialValues={initialValues} onSubmit={onSubmit} validationSchema={validationSchema}>
                     {(props) => (
-                        <Form>
+                        <Form onSubmit={handleOnSubmit}>
                         <FormControl sx={{mt:2,ml:0,  minWidth: 200 }} size="small">
                                 <InputLabel>Role</InputLabel>
                                 <Select
@@ -74,7 +107,7 @@ const Signup = () => {
                                     id="roleId"
                                     value={role}
                                     label="Age"
-                                    onChange={handleChange}
+                                    onChange={(e) => {setRole(e.target.value)}}
                                 >
                                     <MenuItem value={"lecturer"}>Lecturer</MenuItem>
                                     <MenuItem value={"student"}>Student</MenuItem>
@@ -82,9 +115,11 @@ const Signup = () => {
                                 </Select>
                             </FormControl>
                             <Field as={TextField} label='Name' name='name' placeholder="Enter your Name"
-                                helperText={<ErrorMessage name="name" />} fullWidth required />
+                                helperText={<ErrorMessage name="name" />} fullWidth required  onChange={(e) => {setName(e.target.value)}}/>
+
                             <Field as={TextField} label='Email' name='email' placeholder="Enter your Email"
-                                helperText={<ErrorMessage name="email" />} fullWidth required />
+                                helperText={<ErrorMessage name="email" />} fullWidth required  onChange={(e) => {SetEmail(e.target.value)}}/>
+
                             <FormControl style={marginTop}>
                                 <FormLabel id="demo-controlled-radio-buttons-group">Gender</FormLabel>
                                 <RadioGroup
@@ -92,22 +127,31 @@ const Signup = () => {
                                     name="controlled-radio-buttons-group"
                                     style={{ display: 'initial' }}
                                     helperText={<ErrorMessage name="radioBtn" />}
+                                    onChange={(e) => {setGender(e.target.value)}}
                                 >
                                     <Field as={FormControlLabel} value="female" control={<Radio />} name='radioBtn' label="Female" />
                                     <Field as={FormControlLabel} value="male" control={<Radio />} name='radioBtn' label="Male" />
                                 </RadioGroup>
+
                             </FormControl>
+
+
                             <Field as={TextField} label='Phone' placeholder="Enter your Mobile Number" name='phone'
-                                helperText={<ErrorMessage name="phone" style={phnStyle} />} fullWidth required />
+                                helperText={<ErrorMessage name="phone" style={phnStyle} />} fullWidth required  onChange={(e) => {setPhone(e.target.value)}}/>
+
+
                             <Field as={TextField} label='Address' placeholder="Enter your Address" name='address'
-                                helperText={<ErrorMessage name="address" />} fullWidth required />
+                                helperText={<ErrorMessage name="address" />} fullWidth required onChange={(e) => {setAddress(e.target.value)}}/>
+
                             
                             <Field as={TextField} label='Password' placeholder="Enter Passsword" type='password'
-                                helperText={<ErrorMessage name="password" />} name='password' fullWidth required />
+                                helperText={<ErrorMessage name="password" />} name='password' fullWidth required  onChange={(e) => {setPassword(e.target.value)}}/>
+
                             <FormGroup>
                                 <Field as={FormControlLabel} control={<Checkbox defaultChecked name='accept' />} label="I accept the Terms and Condition" helperText={<ErrorMessage name="accept" />} name="accept" />
                                 <Button type="submit" color="primary" variant="contained" disabled={props.isSubmitting} style={btnStyle} fullWidth>{props.isSubmitting ? "Loading..." : "Sign up"}</Button>
                             </FormGroup>
+                            
                         </Form>
                     )}
                 </Formik>
